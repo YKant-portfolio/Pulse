6a310c2ca4b13f05773b0f726ddbc253a71eef4f
Yuriy <ykantakuzin@bk.ru>
2021-07-07 16:54:00 +0300

    I rewrote the tabs and rotation functions of product cards from jquery to javascript

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,17 @@ gulp.task('server', function () {
 
 gulp.task('styles', function () {
 	return gulp.src("src/sass/**/*.+(scss|sass)")
-		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-		.pipe(rename({ suffix: '.min', prefix: '' }))
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(rename({
+			suffix: '.min',
+			prefix: ''
+		}))
 		.pipe(autoprefixer())
-		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(cleanCSS({
+			compatibility: 'ie8'
+		}))
 		.pipe(gulp.dest("dist/css"))
 		.pipe(browserSync.stream());
 });
@@ -35,7 +42,9 @@ gulp.task('watch', function () {
 
 gulp.task('html', function () {
 	return gulp.src("src/*.html")
-		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
 		.pipe(gulp.dest("dist/"));
 });
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,127 @@
-const slider = tns({
-	container: '.carousel__inner',
-	items: 1,
-	slideBy: 'page',
-	autoplay: false,
-	controls: false,
-	nav: true,
-	navPosition: 'bottom',
-	autoHeight: false,
+window.addEventListener('DOMContentLoaded', function () {
+	//slider
+	const slider = tns({
+		container: '.carousel__inner',
+		items: 1,
+		slideBy: 'page',
+		autoplay: false,
+		controls: false,
+		nav: true,
+		navPosition: 'bottom',
+		autoHeight: false,
 
-
-	responsive: {
-		320: {
-			nav: true,
-			navPosition: 'bottom',
-			edgePadding: 20,
-			gutter: 20,
-			items: 1,
-		},
-		991: {
-			nav: false,
+		responsive: {
+			320: {
+				nav: true,
+				navPosition: 'bottom',
+				edgePadding: 20,
+				gutter: 20,
+				items: 1,
+			},
+			991: {
+				nav: false,
+			}
 		}
+	});
+	document.querySelector('.prev').addEventListener('click', function () {
+		slider.goTo('prev');
+	});
+	document.querySelector('.next').addEventListener('click', function () {
+		slider.goTo('next');
+	});
+
+	//tabs
+	const tabs = document.querySelectorAll('.catalog__tab'),
+		tabsClick = document.querySelectorAll('.catalog__click'),
+		catalogContent = document.querySelectorAll('.catalog__content'),
+		parentTabs = document.querySelector('.catalog__tabs');
+
+
+	function hiddenTabContent() {
+		catalogContent.forEach(item => {
+			item.classList.remove('catalog__content-active');
+		});
+		tabs.forEach(tab => {
+			tab.classList.remove('catalog__tab-active');
+		});
 	}
+
+	function showTabContent(i = 1) {
+		catalogContent[i].classList.add('catalog__content-active');
+		tabs[i].classList.add('catalog__tab-active');
+	}
+
+	hiddenTabContent();
+	showTabContent();
+
+	parentTabs.addEventListener('click', function (event) {
+		const target = event.target;
+		if (target && target.classList.contains('catalog__click')) {
+			tabsClick.forEach((item, i) => {
+				if (target == item) {
+					hiddenTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+
+	//product cards toggleSlide
+	const items = document.querySelectorAll('.catalog-item'),
+		itemContent = document.querySelectorAll('.catalog-item__content'),
+		itemList = document.querySelectorAll('.catalog-item__list'),
+		link = document.querySelectorAll('.catalog-item__link'),
+		back = document.querySelectorAll('.catalog-item__back');
+
+	function itemsToggle(i) {
+		itemContent[i].classList.toggle('catalog-item__content-active');
+		itemList[i].classList.toggle('catalog-item__list-active');
+	}
+
+	items.forEach(item => {
+		item.addEventListener('click', function (event) {
+			event.preventDefault();
+			const target = event.target;
+			link.forEach((item, i) => {
+				if (target == item) {
+					itemsToggle(i);
+				}
+			});
+			back.forEach((item, i) => {
+				if (target == item) {
+					itemsToggle(i);
+				}
+			});
+		});
+	});
 });
-document.querySelector('.prev').addEventListener('click', function () {
-	slider.goTo('prev');
-});
-document.querySelector('.next').addEventListener('click', function () {
-	slider.goTo('next');
-});
+
+
+
 
 
 $(document).ready(function () {
-	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab-active)', function () {
-		$(this)
-			.addClass('catalog__tab-active').siblings().removeClass('catalog__tab-active')
-			.closest('div.container').find('div.catalog__content').removeClass('catalog__content-active').eq($(this).index()).addClass('catalog__content-active');
-	});
+	/* 	//the tabs were created using jquery, I rewrote them in javascript line 33
+		$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab-active)', function () {
+			$(this)
+				.addClass('catalog__tab-active').siblings().removeClass('catalog__tab-active')
+				.closest('div.container').find('div.catalog__content').removeClass('catalog__content-active').eq($(this).index()).addClass('catalog__content-active');
+		}); */
 
+	/* 	function toggleSlide(item) {
+			$(item).each(function (i) {
+				$(this).on('click', function (e) {
+					e.preventDefault();
+					$('.catalog-item__list').eq(i).toggleClass('catalog-item__list-active');
+					$('.catalog-item__content').eq(i).toggleClass('catalog-item__content-active');
 
-	function toggleSlide(item) {
-		$(item).each(function (i) {
-			$(this).on('click', function (e) {
-				e.preventDefault();
-				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list-active');
-				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content-active');
-
+				});
 			});
-		});
-	};
-	toggleSlide('.catalog-item__link');
-	toggleSlide('.catalog-item__back');
+		}
+		toggleSlide('.catalog-item__link');
+		toggleSlide('.catalog-item__back'); */
+
+
 
 
 	//Modal
@@ -65,9 +137,11 @@ $(document).ready(function () {
 		$(this).on('click', function () {
 			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
 			$('.overlay, #order').fadeIn('slow');
-		})
+		});
 	});
 
+
+	//validate
 	function validateForms(form) {
 		$(form).validate({
 			rules: {
@@ -87,7 +161,7 @@ $(document).ready(function () {
 				}
 			}
 		});
-	};
+	}
 
 	validateForms('#consultation form');
 	validateForms('#consultation-form');
@@ -123,12 +197,16 @@ $(document).ready(function () {
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 1600) {
 			$('.pageup').fadeIn();
-		} else { $('.pageup').fadeOut(); }
+		} else {
+			$('.pageup').fadeOut();
+		}
 	});
 
 	$("a[href^='#']").click(function () {
 		const _href = $(this).attr("href");
-		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+		$("html, body").animate({
+			scrollTop: $(_href).offset().top + "px"
+		});
 		return false;
 	});
 
